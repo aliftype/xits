45452b1edf0f01757b0aa80e82313d542fdbd34e
Khaled Hosny <khaledhosny@eglug.org>
2011-01-03 19:27:04 +0200

    Rewrite

--- test-suite/sscript.lua
+++ test-suite/sscript.lua
@@ -1,20 +1,12 @@
-
 local format, lower = string.format, string.lower
 
 local alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-local styles = { }
-
-if context then
-    styles = { "it", "bi", "rm", "bf" }
-else
-    styles = { "it", "bfit", "rm", "bf" }
-end
+local styles = { "it", "bi", "rm", "bf" }
 
 for c in alpha:gmatch"." do
-    tex.sprint("$")
     for _,i in next, styles do
-        tex.sprint(format("\\math%s{%s^1_1}\\kern5pt", i, c))
-        tex.sprint(format("\\math%s{%s^1_1}\\kern5pt", i, lower(c)))
+        context.mathematics(format([[\math%s{%s^1_1}\kern5pt]], i, c))
+        context.mathematics(format([[\math%s{%s^1_1}\kern5pt]], i, lower(c)))
     end
-    tex.sprint("$\\blank[big]")
+    context.blank({"big"})
 end
