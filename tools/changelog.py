77aca986ff2ce887ef11f5ff3f007a77a4cb191e
Khaled Hosny <khaledhosny@eglug.org>
2011-11-08 21:16:38 +0200

    Extract version number from Makefile

--- tools/changelog.py
+++ tools/changelog.py
@@ -5,6 +5,7 @@ import datetime
 date = datetime.date.today().strftime("%d %b %Y")
 tag = commands.getoutput("git tag").split("\n")[-1]
 log = commands.getoutput("git log --reverse --pretty=format:'- %%s' %s.. sources" % tag)
-print "%s (Khaled Hosny) <XITS> Version %s" %(date, tag.replace("v", ""))
+version = commands.getoutput("grep -o '^VERSION=.*.$' Makefile | cut --delimiter='=' --fields=2")
+print "%s (Khaled Hosny) <XITS> Version %s" %(date, version)
 print log
 print
