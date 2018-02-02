35fcf094f3c2504ca03c8925536dad8fa11d455d
Khaled Hosny <khaledhosny@eglug.org>
2018-02-02 18:02:16 +0200

    First try at reproducible builds

--- tools/build.py
+++ tools/build.py
@@ -1,8 +1,12 @@
+import os
 import sys
 
 import fontforge
+from fontTools.ttLib import TTFont
 
 infile, outfile, version = sys.argv[1:4]
+
+mods = [os.stat(infile).st_mtime]
 
 font = fontforge.open(infile)
 
@@ -10,7 +14,21 @@ for glyph in font.glyphs():
     glyph.unlinkRmOvrlpSave = True
 
 if len(sys.argv) > 4:
+  mods += [os.stat(sys.argv[4]).st_mtime]
   font.mergeFeature(sys.argv[4])
+
+os.environ["SOURCE_DATE_EPOCH"] = "%d" % max(mods)
+
+font.appendSFNTName("English (US)", "UniqueID", "%s;%s;%s" % (version,
+    font.os2_vendor, font.fontname))
 
 font.version = version
 font.generate(outfile, flags=("round", "opentype"))
+
+ttfont = TTFont(outfile)
+
+# Drop useless table with timestamp
+if "FFTM" in ttfont:
+    del ttfont["FFTM"]
+
+ttfont.save(outfile)
